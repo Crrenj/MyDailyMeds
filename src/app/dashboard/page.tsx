929f92d8e949c11a1bb53f0c546f4329a578831e
crrenj <juanlantiguajorge@gmail.com>
2024-12-11 23:46:43 +0100

    erreur corriger

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,13 +1,12 @@
 "use client";
 
 import Sidebar from "@/components/dashboard/Sidebar";
-import MainContent from "@/components/dashboard/MainContent";
+
 
 export default function DashboardPage() {
   return (
     <div className="flex h-screen">
       <Sidebar />
-      <MainContent />
     </div>
   );
 }
